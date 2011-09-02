9de5d14f019a14032f087722818d8a674a0dbd0e
Tim Jones <github@roastedamoeba.com>
2011-09-03 02:15:16 +0800

    Basic particle physics

---
+++ src/Jolt.Tests/ParticleTests.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using NUnit.Framework;
+using Nexus;
+
+namespace Jolt.Tests
+{
+	[TestFixture]
+	public class ParticleTests
+	{
+		[Test]
+		 public void CanCreateParticle()
+		{
+			Assert.DoesNotThrow(() => new Particle());
+		}
+
+		[Test]
+		public void CanSetAndGetParticleMass()
+		{
+			// Arrange.
+			var particle = new Particle();
+
+			// Act.
+			particle.Mass = 2f;
+
+			// Assert.
+			Assert.That(particle.Mass, Is.EqualTo(2));
+		}
+
+		[Test]
+		public void SettingParticleMassToInvalidValueThrowsException()
+		{
+			// Arrange.
+			var particle = new Particle();
+
+			// Act / Assert.
+			Assert.Throws<ArgumentOutOfRangeException>(() => particle.Mass = 0);
+		}
+
+		[Test]
+		public void ParticleMassIsInfinityWhenInverseMassIsZero()
+		{
+			// Arrange.
+			var particle = new Particle();
+
+			// Act.
+			particle.InverseMass = 0f;
+
+			// Assert.
+			Assert.That(particle.Mass, Is.EqualTo(float.PositiveInfinity));
+		}
+
+		[Test]
+		public void ParticleIntegrationDoesNothingWhenNoForces()
+		{
+			// Arrange.
+			var particle = new Particle();
+			particle.Mass = 2f;
+
+			// Act.
+			particle.Integrate(1.0f);
+
+			// Assert.
+			Assert.That(particle.Position, Is.EqualTo(Point3D.Zero));
+			Assert.That(particle.Velocity, Is.EqualTo(Vector3D.Zero));
+			Assert.That(particle.Acceleration, Is.EqualTo(Vector3D.Zero));
+		}
+
+		[Test]
+		public void ParticleIntegrationAltersValuesCorrectly()
+		{
+			// Arrange.
+			var particle = new Particle();
+			particle.Mass = 2f;
+			particle.Damping = 0.99f;
+			particle.AddForce(Vector3D.Forward);
+
+			// Act.
+			particle.Integrate(1.0f);
+
+			// Assert.
+			Assert.That(particle.Position, Is.EqualTo(Point3D.Zero));
+			Assert.That(particle.Velocity, Is.EqualTo(new Vector3D(0, 0, -0.495f)));
+			Assert.That(particle.Acceleration, Is.EqualTo(Vector3D.Zero));
+		}
+	}
+}
---
+++ src/Jolt/Particle.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Nexus;
+
+namespace Jolt
+{
+	public class Particle
+	{
+		private Vector3D _forceAccumulator;
+
+		public float Mass
+		{
+			get
+			{
+				return (InverseMass == 0.0f) 
+					? float.PositiveInfinity 
+					: 1.0f / InverseMass;
+			}
+			set
+			{
+				if (value == 0.0f)
+					throw new ArgumentOutOfRangeException("value", "Mass cannot be 0.");
+				InverseMass = 1.0f / value;
+			}
+		}
+
+		public float InverseMass
+		{
+			get;
+			set;
+		}
+
+		public bool HasFiniteMass
+		{
+			get { return InverseMass >= 0.0f; }
+		}
+
+		public float Damping { get; set; }
+		public Point3D Position { get; set; }
+		public Vector3D Velocity { get; set; }
+		public Vector3D Acceleration { get; set; }
+
+		public void AddForce(Vector3D force)
+		{
+			_forceAccumulator += force;
+		}
+
+		public void Integrate(float duration)
+		{
+			// Don't integrate things with zero mass.
+			if (InverseMass <= 0.0f)
+				return;
+
+			if (duration <= 0.0f)
+				throw new ArgumentOutOfRangeException("duration", "Duration must be greater than 0.");
+
+			// Update linear position.
+			Position += Velocity * duration;
+
+			// Calculate the acceleration from the force.
+			Vector3D resultingAcceleration = Acceleration;
+			resultingAcceleration += _forceAccumulator * InverseMass;
+
+			// Update linear velocity from the acceleration.
+			Velocity += resultingAcceleration * duration;
+
+			// Impose drag.
+			Velocity *= MathUtility.Pow(Damping, duration);
+
+			// Clear the forces.
+			ClearAccumulator();
+		}
+
+		private void ClearAccumulator()
+		{
+			_forceAccumulator = Vector3D.Zero;
+		}
+	}
+}
